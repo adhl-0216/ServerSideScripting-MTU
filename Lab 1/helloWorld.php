c8a6865241cbe404bf28b4889980f2e4962cd529
ADHL <Han.Lim.Oah@students.ittralee.ie>
2023-01-26 12:31:51 +0000

    lab 3

--- Lab 1/helloWorld.php
+++ Lab 1/helloWorld.php
@@ -8,12 +8,12 @@
     <meta name='author' content=''>
     <meta name='robots' content='all'>
     <!-- <meta http-equiv='X-UA-Compatible' content='IE=edge'> -->
-    <link href='/favicon.png' rel='shortcut icon' type='image/png'>
   </head>
   <body>
 <?php
-    print ("<h2> Adrian </h2>");
-    print( "<h2> 113, TTCA </h2>");
+date_default_timezone_set("Asia/Seoul");
+echo "<h1>Hello, World!</h1>";
+echo "<p>Today's date in Seoul is ".date("l, F dS Y. (H:m)")."</p>";
 ?>
   </body>
 </html>
